296ba2794de94b73716266b09a7a8857eea8f2ec
Caleb Miller <calebgmiller@gmail.com>
2018-03-18 21:35:37 -0700

    Add files via upload

--- MazeProject/src/Main.cpp
+++ MazeProject/src/Main.cpp
@@ -2,7 +2,7 @@
  * Main.cpp
  *
  *  Created on: Mar 11, 2018
- *      Author: gage_
+ *      Author: Caleb Miller
  */
 
 #include <iostream>
@@ -14,40 +14,34 @@
 
 using namespace std;
 
-int main ()
-{
-    // Create array to hold input file
+int main () {
 	string txt[12];
 	string line;
 
 	int i=0;
-	ifstream file ("MazeProject\src\input_file.txt"); // Get file
+	ifstream file ("input_file.txt");
 	if (file.is_open()){
 		while (!file.eof()){
 			getline(file,line);
-			txt[i]=line; // For each line in file add to text array
+			txt[i]=line;
 			i++;
 		}
 	}
 	file.close();
 
-    // Create file for output
     ofstream myfile;
     myfile.open ("maze_project.txt");
 
-    // Create height and width
     int height = 11;
     int width = 10;
 
-    // Create string for different blocks
     string open = "1";
-    string start = "s";
-    string free = "f";
+    string start = "S";
+    string free = "F";
     string wall = "0";
 
-    // Create array of maze
 string blockArray[11][10] = {
-		{"s","1","1","1","0","1","1","1","1","1"},
+		{"S","1","1","1","0","1","1","1","1","1"},
 		{"1","0","0","0","1","1","0","0","0","1"},
 		{"1","0","1","0","1","0","1","0","1","1"},
 		{"1","1","1","0","1","0","1","0","1","0"},
@@ -57,11 +51,10 @@ string blockArray[11][10] = {
 		{"0","0","0","0","1","0","1","1","1","0"},
 		{"1","1","0","1","1","1","0","1","0","1"},
 		{"0","1","1","1","0","1","0","1","1","1"},
-		{"1","1","1","f","0","1","1","1","0","1"},
+		{"1","1","1","F","0","1","1","1","0","1"},
 	};
 
 
-        // Iterates through the rows of the blockArray
         for (int i = 0; i < height; i++) {
 
             // run the first time to create numbers
@@ -81,21 +74,17 @@ string blockArray[11][10] = {
             // run 3 times to create 3x3 block
             for (int h = 0; h < 3; h ++) {
 
-                // is 1st row in block
                 if(h == 1 && i < 10){
                     myfile << i << " |";
                 }
-                // is 2nd row in block
                 else if (h == 1 && i >= 10){
                     myfile << i << "|";
                 }
-                // is 3rd row in block
                 else {
                     myfile << "  |";
                 }
 
                 for (int j = 0; j < width; j++) {
-					// run 3 times for 303 block
 					for (int k = 0; k < 3; k++) {
 						if (k != 1) {
 							if (blockArray[i][j] == start) {
